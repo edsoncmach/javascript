a6f976d11c44d9f056a0424f80a1c5fe3b82230a
edsoncmach <tec.info.edson@gmail>
2024-07-15 17:40:13 -0300

    exercicios de map, filter e reduce javascript

---
+++ exercicios7.js
@@ -0,0 +1,27 @@
+// exercício 1: acrescentar 10% os valores de market.
+const companies = [
+    {name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938},
+    {name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975},
+    {name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968},
+    {name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004},
+    {name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006},
+    {name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976}
+]
+
+const newValuesCompanies = companies.map((company)  => {
+    company.marketValue += company.marketValue * 1.1    
+    
+    return company
+})
+
+const oldCompanies = newValuesCompanies.filter( (company) => company.foundedOn < 2000)
+
+const sumOldCompanies = oldCompanies.reduce((acumulator, currentValue) => {
+    return acumulator + currentValue.marketValue
+}, 0)
+
+
+console.log(`Total: $ ${sumOldCompanies.toFixed(2)}`)
+
+
+
